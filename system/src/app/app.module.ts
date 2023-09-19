6a7ccfac847f15a7f88d44ba893223f1ba465205
Nathaniel Nguyen <nathaniel.nguyen@unc.edu>
2023-09-18 21:40:21 -0400

    Added the Order component.

--- system/src/app/app.module.ts
+++ system/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
